cd7e6af96d321bc62fdc6107ee4cf8268d9bcae8
ChiefOfGxBxL <langgb@clarkson.edu>
2021-09-24 15:22:52 -0400

    Add counterfactual to landing page example

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
         <h3 class="title is-5">Article 1</h3>
         <h4 class="subtitle is-6"><a @click="tryExample1">Try it out live &raquo;</a></h4>
         <p>
-          A 35-year-old <mark class="focus">woman was hit by a car</mark> in Queens on Saturday, August 28th. The <mark class="focus">woman was run over at about 5:50 a.m. by a grey or silver SUV</mark> at the intersection of Hillside Avenue and Queens Boulevard in the Jamaica neighborhood of Queens. The driver fled the scene. Surveillance video found online shows the victim, unsteady on her feet, falling over onto Queens Boulevard. Moments later, the video shows an <mark class="object">SUV running over the pedestrian</mark>, pulling her under its wheels. The driver kept going. The unidentified woman was transported to Jamaica Hospital in critical condition. The <mark class="accident">accident</mark> is under investigation.
+          A 35-year-old <mark class="focus">woman was hit by a car</mark> in Queens on Saturday, August 28th. The <mark class="focus">woman was run over at about 5:50 a.m. by a grey or silver SUV</mark> at the intersection of Hillside Avenue and Queens Boulevard in the Jamaica neighborhood of Queens. The driver fled the scene. Surveillance video found online shows the victim, unsteady on her feet, falling over onto Queens Boulevard. Moments later, the video shows an <mark class="object">SUV running over the pedestrian</mark>, pulling her under its wheels. The driver kept going. The unidentified woman was transported to Jamaica Hospital in critical condition. The <mark class="accident">accident</mark> is under investigation. <mark class="counter">Alcohol does not appear to be a factor</mark>.
         </p>
       </div>
       <div class="column">
@@ -47,7 +47,7 @@ export default {
       this.$router.push({ path: 'report', query: { title , body } })
     },
     tryExample1() {
-      this.goToReportPage('Queens crash leaves one injured', 'A 35-year-old woman was hit by a car in Queens on Saturday, August 28th. The woman was run over at about 5:50 a.m. by a grey or silver SUV at the intersection of Hillside Avenue and Queens Boulevard in the Jamaica neighborhood of Queens. The driver fled the scene. Surveillance video found online shows the victim, unsteady on her feet, falling over onto Queens Boulevard. Moments later, the video shows an SUV running over the pedestrian, pulling her under its wheels. The driver kept going. The unidentified woman was transported to Jamaica Hospital in critical condition. The accident is under investigation.')
+      this.goToReportPage('Queens crash leaves one injured', 'A 35-year-old woman was hit by a car in Queens on Saturday, August 28th. The woman was run over at about 5:50 a.m. by a grey or silver SUV at the intersection of Hillside Avenue and Queens Boulevard in the Jamaica neighborhood of Queens. The driver fled the scene. Surveillance video found online shows the victim, unsteady on her feet, falling over onto Queens Boulevard. Moments later, the video shows an SUV running over the pedestrian, pulling her under its wheels. The driver kept going. The unidentified woman was transported to Jamaica Hospital in critical condition. The accident is under investigation. Alcohol does not appear to be a factor.')
     },
     tryExample2() {
       this.goToReportPage('Bensonhurst accident kills one', 'A bicyclist was hit and killed by a car in the Bensonhurst area on Friday morning, August 27th. The fatal accident happened at the intersection of 16th Avenue and 85th Street around 10:04 a.m. According to local authorities, the woman was attempting to cross at the intersection when she was struck. Police and EMTs responded to the scene and transported Pan to Maimonides Medical Center. She was pronounced deceased a short time later. A preliminary investigation determined that the victim was crossing 16th Avenue when the pickup struck her while making a left turn. The 36-year-old male driver of the truck has not been arrested at this time.')
@@ -65,4 +65,5 @@ mark.focus { background-color: rgba(214, 81, 41, 0.3); }
 mark.agency { background-color: rgba(31, 179, 224, 0.3); }
 mark.object { background-color: rgba(73, 194, 25, 0.3); }
 mark.accident { background-color: rgba(238, 180, 23, 0.3); }
+mark.counter { background-color: rgba(244, 220, 42, 0.3); }
 </style>
