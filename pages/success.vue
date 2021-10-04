e1f8216328cdd8ecab79681f96e7ed72e911f508
ChiefOfGxBxL <langgb@clarkson.edu>
2021-10-04 17:25:42 -0400

    Add custom form submission success page

---
+++ pages/success.vue
@@ -0,0 +1,28 @@
+<template>
+    <article>
+        <section class="content">
+            <h3 class="title is-4">
+                <b-icon icon="check-decagram" size="is-large" type="is-success" /><br/>
+                Your form was successfully submitted!
+            </h3>
+            <p>We'll review your message and respond to you soon. Thanks for reaching out.</p>
+        </section>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'Contact'
+}
+</script>
+
+<style scoped>
+section {
+    border-top: 3px SOLID rgb(70, 200, 140);
+    background-color: #f7f7f7;
+    padding: 20px;
+    border-radius: 0px;
+    text-align: center;
+}
+label > input[type="radio"] { margin-left: 15px; }
+</style>
