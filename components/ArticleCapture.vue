98e1473684bcd6e37f2576c37d73e638e89da021
ChiefOfGxBxL <langgb@clarkson.edu>
2021-10-19 13:26:15 -0400

    Accept URL for article input method
    URL is retrieved on the backend, parsed, and evaluated.

--- components/ArticleCapture.vue
+++ components/ArticleCapture.vue
@@ -1,27 +1,24 @@
 <template>
     <div class="article-capture-container">
-        <h3 class="subtitle is-5">Copy and paste the content of a crash-related news article below</h3>
-        <b-field label="Title">
-            <b-input v-model="title" placeholder="One injured in Queens car accident"></b-input>
-        </b-field>
-        <b-field label="Body">
-            <b-input type="textarea" v-model="body" placeholder="A 35-year-old woman was hit by a car in Queens on Saturday, August 28th. The woman was run over at about 5:50 a.m. by a grey or silver SUV at the intersection of Hillside Avenue and Queens Boulevard in the Jamaica neighborhood of Queens."></b-input>
-        </b-field>
-        <!-- <b-tabs v-model="selectedTabIndex">
-            <b-tab-item label="Text" icon="file-document-outline">
-                <b-field label="Title">
-                    <b-input v-model="title"></b-input>
-                </b-field>
-                <b-field label="Body">
-                    <b-input type="textarea" v-model="body"></b-input>
-                </b-field>
-            </b-tab-item>
-            <b-tab-item label="URL" icon="link">
-                <b-field label="URL">
-                    <b-input v-model="url" placeholder="https://"></b-input>
-                </b-field>
-            </b-tab-item>
-        </b-tabs> -->
+
+        <b-tabs v-model="selectedTabIndex">
+          <b-tab-item label="URL" icon="link">
+            <h3 class="subtitle is-5">Paste the URL of a crash-related news article below</h3>
+            <b-field label="URL">
+              <b-input v-model="url" placeholder="https://"></b-input>
+            </b-field>
+          </b-tab-item>
+          <b-tab-item label="Manual entry" icon="file-document-outline">
+            <h3 class="subtitle is-5">Copy and paste the content of a crash-related news article below</h3>
+            <b-field label="Title">
+              <b-input v-model="title" placeholder="One injured in Queens car accident"></b-input>
+            </b-field>
+            <b-field label="Body">
+              <b-input type="textarea" v-model="body" placeholder="A 35-year-old woman was hit by a car in Queens on Saturday, August 28th. The woman was run over at about 5:50 a.m. by a grey or silver SUV at the intersection of Hillside Avenue and Queens Boulevard in the Jamaica neighborhood of Queens."></b-input>
+            </b-field>
+          </b-tab-item>
+        </b-tabs>
+
         <b-button type="is-primary" @click="submitButtonClick" class="is-pulled-right" icon-left="file-search-outline">Analyze</b-button>
         <p style="clear:both;"></p>
     </div>
@@ -36,30 +33,19 @@ export default {
       url: '',
       title: '',
       body: '',
-      errorMessage: ''
     }
   },
   methods: {
     submitButtonClick() {
       // if the user provided a URL, we need to fetch the content
-      if(this.$data.selectedTabIndex === 1) {
-        // this.$axios.get(this.$data.url)
-        //     .then(response => {
-        //         const articleData = unfluff(response.data)
-        //         console.log(articleData)
-        //     })
-        //     .catch(error => {
-        //         console.error(error)
-        //     })
-      }
-      else { // user manually entered title and body
-        const { title, body } = this.$data
-        this.goToReportPage(title, body)
-      }
+      const { url, title, body } = this.$data
+      this.goToReportPage(url, title, body)
     },
-    goToReportPage(title, body) {
-      if(title && body) {
-        this.$router.push({ path: 'report', query: { title , body } })
+    goToReportPage(url, title, body) {
+      const query = url ? { url } : { title, body }
+
+      if(url || (title && body)) {
+        this.$router.push({ path: 'report', query })
       }
     }
   }
