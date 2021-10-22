56f7bff1c122bd837d102d22c0e56b64e4a4a026
ChiefOfGxBxL <langgb@clarkson.edu>
2021-10-22 11:39:35 -0400

    Add highlights to each section
    Refactors each category on the report page into a re-usable component
    
    Having a highlight in each section helps user not having to scroll up for reference, and eliminates the need for color-coding which can be an accessibility issue

--- pages/report.vue
+++ pages/report.vue
@@ -16,62 +16,7 @@
       <div v-else>
         <ol v-if="problems.length">
           <div class="problem-list" v-for="category in problemsCategorizedNonEmpty" :key="category.name">
-            <b-collapse class="card" animation="slide" aria-id="contentIdForA11y3">
-              <template #trigger="props">
-                <div
-                  class="card-header"
-                  role="button"
-                  aria-controls="contentIdForA11y3">
-                  <p class="card-header-title">
-                    <b-icon :icon="category.icon" />
-                    <span>{{category.name}}</span>
-                    <small>{{category.count}} {{category.count == 1 ? 'issue' : 'issues'}}</small>
-                  </p>
-                  <a class="card-header-icon">
-                    <b-tag type="is-warning" v-if="category.isExperimental">
-                      <b-icon icon="flask-outline" size="is-small" />
-                      Experimental
-                    </b-tag>
-                    <b-icon :icon="props.open ? 'menu-down' : 'menu-up'" />
-                  </a>
-                </div>
-              </template>
-
-              <div class="card-content">
-                <h5 class="subtitle is-6">
-                  {{category.issue}}.
-                  {{category.explain}}
-                  <nuxt-link :to="{ path: '/issues', hash: category.name.toLowerCase() }">[Learn more about {{category.name.toLowerCase()}}]</nuxt-link>
-                </h5>
-
-                <section>
-                  <label>Sentences</label>
-                  <ol v-if="category.problems.length">
-                    <li v-for="problem in category.problems" :key="problem">
-                      <q>{{problem.sentence}}</q>
-                      <b-tag type="is-warning is-light" v-if="problem.details && category.id == 'COUNTER'">
-                        {{Counterfactuals[problem.details].name}}
-                      </b-tag>
-                    </li>
-                  </ol>
-                  <ul v-else>
-                    <li>None found.</li> <!-- for Framing reporter -->
-                  </ul>
-                </section>
-
-                <section>
-                  <label>How to fix</label>
-                  <p>{{category.fix}}</p>
-                </section>
-
-                <section v-if="category.examples">
-                  <label>Examples</label>
-                  <ul>
-                    <li v-for="example in category.examples" :key="example.text" class="example" :class="example.type">{{example.text}}</li>
-                  </ul>
-                </section>
-              </div>
-            </b-collapse>
+            <problem-category :category="category" :article="article" />
           </div>
         </ol>
         <div v-else class="empty">
@@ -89,12 +34,12 @@
 
 <script>
 import ProblemTypes from '../assets/ProblemTypes'
-import Counterfactuals from '../assets/Counterfactuals'
 import ReportSummary from '../components/ReportSummary.vue'
+import ProblemCategory from '../components/ProblemCategory.vue'
 
 export default {
   name: 'Report',
-  components: { ReportSummary },
+  components: { ReportSummary, ProblemCategory },
   data() {
     return {
       isLoading: true,
@@ -106,7 +51,6 @@ export default {
       },
       problems: [],
       ProblemTypes,
-      Counterfactuals
     }
   },
   methods: {
@@ -187,49 +131,9 @@ section.primary { margin: 40px 0; }
   border-radius: 5px;
 }
 
-small { font-style: italic; padding-left: 10px; }
 h1 { width: 70%; }
 .empty { background-color: #f9f9f9; padding: 20px; text-align: center; }
 
 div.level-item { background-color: #e5e5e5; }
 p.title { font-size: 1.2em; }
-
-.card-header-title span:not([class^="icon"]) {
-  margin-left: 4px;
-}
-.card-header-title small { font-weight: normal; font-style: normal; }
-.problem-list h5.subtitle { border-bottom: 0.75rem; }
-.problem-list ol { margin-left: 15px; }
-.problem-list q {
-  padding: 3px;
-  background-color: #f2f2f2;
-  font-style: italic;
-}
-
-.card-content section {
-  padding-left: 15px;
-  margin-top: 15px;
-}
-.card-content section label {
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-weight: bold;
-  font-size: 0.7em;
-  color: #777;
-  display: inline-block;
-  margin-bottom: 6px;
-  position: relative;
-  left: -15px;
-}
-
-ol li {
-  margin: 6px 0;
-}
-
-li.example::before {
-  color: #444;
-  margin: 0 6px 0 0;
-}
-li.good::before { content: '\2713' }
-li.bad::before { content: '\2A2F' }
 </style>
