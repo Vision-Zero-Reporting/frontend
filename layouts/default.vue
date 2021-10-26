034cb301c8a97a80db4d8f488338569e5f567ee3
ChiefOfGxBxL <langgb@clarkson.edu>
2021-10-26 17:16:23 -0400

    Left-align main nav links

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,20 +9,17 @@
           >
         </b-navbar-item>
       </template>
-      <template #end>
+      <template #start>
         <!-- See: https://stackoverflow.com/questions/57358490/how-to-use-nuxt-link-tag-in-buefy -->
-        <nuxt-link to="/issues" class="navbar-item">
-          Issues
-        </nuxt-link>
+        <nuxt-link to="/" class="navbar-item">Home</nuxt-link>
+        <nuxt-link to="/issues" class="navbar-item">Issues</nuxt-link>
+      </template>
+      <template #end>
         <!-- <nuxt-link to="/partners" class="navbar-item">
           Partners
         </nuxt-link> -->
-        <nuxt-link to="/contact" class="navbar-item">
-          Contact
-        </nuxt-link>
-        <nuxt-link to="/about" class="navbar-item">
-          About
-        </nuxt-link>
+        <nuxt-link to="/contact" class="navbar-item">Contact</nuxt-link>
+        <nuxt-link to="/about" class="navbar-item">About</nuxt-link>
         <a class="navbar-item" href="https://twitter.com/ZeroReporting" target="_blank">
           <b-icon icon="twitter" size="is-small" type="is-info" />
         </a>
