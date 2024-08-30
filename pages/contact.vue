38590f4bbdea239e2b0cecbcc71ce9b097404c02
ChiefOfGxBxL <langgb@clarksonalumni.com>
2024-08-30 15:51:15 -0400

    Remove unused Netlify form code

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,48 +4,6 @@
         <h2 class="subtitle is-4">Get in touch</h2>
         <section class="content">
           For questions, feedback, or assistance, email us at <a href="mailto:visionzeroreporting@gmail.com">visionzeroreporting@gmail.com</a>
-            <!-- <form name="contact" method="POST" action="/success/" netlify>
-                <p>
-                    <label>Full name:
-                        <input type="text" name="name" class="input" />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Email address:
-                        <input type="email" name="email" class="input" />
-                    </label>
-                </p>
-                <p>
-                    Pick the role that best describes you:<br/>
-                    <label>
-                        <input type="radio" name="role" value="journalist" />
-                        Journalist / news publisher
-                    </label><br/>
-                    <label>
-                        <input type="radio" name="role" value="researcher" />
-                        Researcher
-                    </label><br/>
-                    <label>
-                        <input type="radio" name="role" value="government" />
-                        Government official
-                    </label><br/>
-                    <label>
-                        <input type="radio" name="role" value="other" checked />
-                        Individual
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Message:
-                        <textarea name="message" class="textarea"></textarea>
-                    </label>
-                </p>
-                <p>
-                    <button type="submit" class="button is-info is-pulled-right">Submit</button>
-                </p>
-                <div style="clear:both;"></div>
-            </form> -->
         </section>
     </article>
 </template>
