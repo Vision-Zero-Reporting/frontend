1ea403d01778e7d5c0f02f101ad700d3aae09225
ChiefOfGxBxL <langgb@clarksonalumni.com>
2024-09-05 15:17:07 -0400

    Add validation to registration form and submit

--- pages/register.vue
+++ pages/register.vue
@@ -6,6 +6,14 @@
         Failed to register. Please check that all fields are filled out properly. If this error persists,
         please contact support at <a href="mailto:visionzeroreporting@gmail.com">visionzeroreporting@gmail.com</a>
       </b-message>
+      <b-message v-if="validationErrors.length" type="is-warning">
+        The following issues should be addressed to register:
+        <ul>
+          <li v-for="errorMsg in validationErrors">{{ errorMsg }}</li>
+        </ul>
+        Please check that all fields are filled out properly. If you need help,
+        please contact support at <a href="mailto:visionzeroreporting@gmail.com">visionzeroreporting@gmail.com</a>
+      </b-message>
       <b-field label="First name">
         <b-input
           type="text"
@@ -59,7 +67,7 @@
         <b-radio v-model="role" native-value="JOURNALIST">
           <b-icon icon="notebook-edit-outline" size="is-small" />
           <strong>Journalism</strong>
-          <p>I am a journalist, publisher, editor, or blogger</p>
+          <p>I am a journalist, publisher, or editor</p>
         </b-radio>
       </b-field>
       <b-field>
@@ -95,6 +103,7 @@ export default {
   data() {
     return {
       failedRegistration: false,
+      validationErrors: [],
       firstName: '',
       lastName: '',
       email: '',
@@ -104,16 +113,45 @@ export default {
     }
   },
   methods: {
+    validate() {
+      const validationErrors = []
+      const { firstName, lastName, email, password, confirmPassword, role } = this.$data
+
+      // Presence of data
+      if (!firstName) validationErrors.push('Please enter your first name')
+      if (!lastName) validationErrors.push('Please enter your last name')
+      if (!email) validationErrors.push('Please enter your email address')
+      if (!password) validationErrors.push('Please enter a password')
+      if (!confirmPassword) validationErrors.push('Please confirm your password')
+      if (!role) validationErrors.push('Please select a role')
+
+      // Email format (see https://emailregex.com/)
+      const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+      if (!email.match(emailRegex)) validationErrors.push('Please enter a valid email')
+
+      // Password checks
+      if (password && confirmPassword && password !== confirmPassword) validationErrors.push('Passwords do not match')
+      if (password.length < 12) validationErrors.push('Password must be at least 12 characters long')
+
+      this.validationErrors = validationErrors
+      return validationErrors.length === 0
+    },
     submit() {
-      // const { email, password } = this.$data
-      // console.log(this.$store.state.user)
-      // this.$axios.post('/login', { email, password })
-      // .then((response) => {
-      //   this.$store.dispatch('checkSession')
-      // })
-      // .catch((e) => {
-      //   this.failedLogin = true
-      // })
+      if (!this.validate()) return
+
+      const { firstName, lastName, email, password, confirmPassword, role } = this.$data
+      this.$axios.post('/user', { firstName, lastName, email, password, confirmPassword, role })
+      .then((response) => {
+        if (response.ok) {
+          this.$store.dispatch('checkSession')
+          // TODO: redirect to dashboard
+        } else {
+          throw Error('Something went wrong during registration')
+        }
+      })
+      .catch((e) => {
+        this.failedRegistration = true
+      })
     }
   }
 }
