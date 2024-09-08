ec246433fa5a7deb0d8bdc12831ef7af86fa242b
ChiefOfGxBxL <langgb@clarksonalumni.com>
2024-09-08 16:31:41 -0400

    Add placeholder cards for future features on dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    <center>
-      <h1 class="title is-2">Welcome, {{ firstName }}</h1>
-    </center>
+    <h1 class="title is-2">Welcome, {{ firstName }}</h1>
 
     <br/>
 
     <ArticleCapture />
 
     <br />
+    <br />
 
+    <h2 class="title is-4 lined">Additional resources</h2>
+    <div class="columns">
+      <div v-for="card in cards" class="column">
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">
+                  <b-icon :icon="card.icon" />
+                  {{ card.title }}
+                </p>
+                <b-tag type="is-primary is-light">Coming soon!</b-tag>
+              </div>
+            </div>
+
+            <div class="content">
+              {{ card.description }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +45,23 @@ export default {
   },
   data() {
     return {
+      cards: [
+        {
+          title: 'Statistics',
+          icon: 'chart-bar',
+          description: 'See an overview of key metrics based on all the articles we\'ve audited!'
+        },
+        {
+          title: 'Press toolkit',
+          icon: 'notebook-edit-outline',
+          description: 'Download an easy-to-use checklist and other resources for writing news articles'
+        },
+        {
+          title: 'API Access',
+          icon: 'cogs',
+          description: 'For researchers and publishers, programatically assess articles using our software'
+        }
+      ]
     }
   },
   computed: {
