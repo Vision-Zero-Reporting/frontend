9482528c5ee09228c9cfb1ebb7cd7c7175c4911d
ChiefOfGxBxL <langgb@clarksonalumni.com>
2024-09-11 20:33:43 -0400

    Align frontend regex with what's used on backend

--- pages/register.vue
+++ pages/register.vue
@@ -135,8 +135,8 @@ export default {
       if (!confirmPassword) validationErrors.push('Please confirm your password')
       if (!role) validationErrors.push('Please select a role')
 
-      // Email format (see https://emailregex.com/)
-      const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+      // See: https://github.com/abdielbytes/regex-gen/blob/main/validator/build/lib/app/email_in_validate.py
+      const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[a-zA-Z0-9-]$/gi
       if (!email.match(emailRegex)) validationErrors.push('Please enter a valid email')
 
       // Password checks
